8bf50840bf3c277a5f1cc8087d93282c04022f09
Luiz <luizgustav796@gmail.com>
2025-04-15 23:39:40 -0300

    impar/par
    impar/par

---
+++ desafio/impar ou par.js
@@ -0,0 +1,8 @@
+function verificar(num1) {
+    num1 = document.getElementById("num1").value;
+    if (numero % 2 === 0) {
+        resultado.textContent = `O número ${numero} é PAR.`;
+      } else {
+        resultado.textContent = `O número ${numero} é ÍMPAR.`;
+      }
+}
