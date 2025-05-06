148eb79961de0e78df3194e3c0bbeb8122e5c90c
Vitor Alexandre Lusni <luizgustav796@gmail.com>
2025-05-06 10:52:15 -0300

    Aula18-php
    Aula18-php

---
+++ aula18-php/taskPHP/index.php.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>meu primeiro programa</title>
+</head>
+<body>
+    <h1>
+        <php></php>
+    </h1>
+
+</body>
+</html>
