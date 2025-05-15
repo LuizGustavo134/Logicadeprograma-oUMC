c57e929aeb9b574ab3ce90ada30b1b3df9bdf137
VilchesBreno <luizgustav796@gmail.com>
2025-05-15 10:57:16 -0300

    aula20
    aula20

---
+++ aula20/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home page</title>
+    <link rel="stylesheet" href="estilos/style.css">
+</head>
+<body>
+    <h1>Bem-vindo ao sistema de cadastro</h1>
+</body>
+<header>
+<nav>
+<ul>
+    <li><a href="index.php">incio</a></li>
+    <li><a href="">cadastrar</a></li>
+    <li><a href="/verificarcadastro.php"> verificar cadastro</a></li>
+</ul>
+</nav>
+</header>
+
+</html>
